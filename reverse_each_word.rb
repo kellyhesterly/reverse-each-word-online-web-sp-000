505b0b6cc76bdcb1d12d6b93cd1b8124bb8dfa94
Kelly Hesterly <kelly.hesterly@gmail.com>
2021-01-29 18:21:30 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,9 +1,15 @@
 require 'pry'
 
-def reverse_each_word(sentence)
-  new_sentence = sentence.
-  # new_sentence.
-  # sentence.each_char do |word|
-  # new_sentence << word.reverse
-  # end
+# def reverse_each_word(sentence)
+#   new_sentence = sentence.
+#   # new_sentence.
+#   # sentence.each_char do |word|
+#   # new_sentence << word.reverse
+#   # end
+# end
+
+def reverse_each_word(str)
+  arr = str.split(" ")
+  new_arr = arr.collect{|a| a.reverse}
+  new_arr.join(" ")
 end
